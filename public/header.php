29b48060beadd082f38b7bc7cd3e9bd2c83803f3
Loann Meignant <loann.meignant@hotmail.fr>
2017-10-05 18:43:39 +0200

    Ajout du header et quelques propriétés css

--- public/header.php
+++ public/header.php
@@ -15,14 +15,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Karuka</title>
 
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 
     <title>Hackathon</title>
     <meta charset="utf-8">
+
 </head>
 
 
 <body>
+
+<nav class="navbar navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <img src="img/e7a9a0a3e7f001893c6b4a3594fa376e.png" class="logoHeader">
+        </div>
+        <div class="collapse navbar-collapse pull-right">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#" class="navbarFontColor">Home</a></li>
+                <li><a href="#about" class="navbarFontColor">About</a></li>
+                <li><a href="#contact" class="navbarFontColor">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
--- public/index.php
+++ public/index.php
@@ -2,10 +2,17 @@
 include 'header.php';
 ?>
 
-
-
-
-
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Welcome to the Music Books !</h1>
+            <p>
+                <a class="btn btn-primary btn-lg">Log in</a>
+            </p>
+            <p>
+                <a class="btn btn-danger btn-lg" href="logout.php">Log out</a>
+            </p>
+        </div>
+    </div>
 
 <?php
 include 'footer.php';
