e4ae62d7460022b4a324f2297ec1850075fb474e
Kezeksa <clay77@hotmail.fr>
2017-10-05 17:25:07 +0200

    Le background d'Emmanuelle ne marche pas

--- public/index.php
+++ public/index.php
@@ -2,11 +2,18 @@
 include 'header.php';
 ?>
 
-
-
-
-
-
+<div class="bookHome">
+  <h1 class="titles">Livres</h1>
+</div>
+<div class="musicHome">
+  <h1 class="titles">Musiques</h1>
+</div>
+<!--
+<script>
+$( document ).ready(function() {
+});
+</script>
+-->
 <?php
 include 'footer.php';
 ?>
