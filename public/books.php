51238e5d5fcb3d6d9ecfb1fb739b337c59f3a6a1
RequinC <em.leviavant@gmail.com>
2017-10-05 20:14:35 +0200

    ajout des thumbnails books et music

--- public/books.php
+++ public/books.php
@@ -5,3 +5,28 @@
  * Date: 05/10/17
  * Time: 15:52
  */
+include 'header.php';
+    ?>
+    /**
+    * Created by PhpStorm.
+    * User: wilder12
+    * Date: 05/10/17
+    * Time: 15:51
+    */
+
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="..." alt="...">
+                <div class="caption">
+                    <h3>Titre du livre</h3>
+                    <p>...</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Add</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+include 'footer.php'
+?>
--- public/music.php
+++ public/music.php
@@ -1,7 +1,26 @@
 <?php
+include 'header.php'
+    ?>
 /**
  * Created by PhpStorm.
  * User: wilder12
  * Date: 05/10/17
  * Time: 15:51
  */
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="..." alt="...">
+      <div class="caption">
+        <h3>Titre de l'album</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Add</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+include 'footer.php'
+?>
