e9a35428187c6e101deab37d1379a71d6eb43efe
Loann Meignant <loann.meignant@hotmail.fr>
2017-10-05 16:35:34 +0200

    Ajout des sous-dossiers et modification du contenu header.php et footer.php

---
+++ public/header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder12
+ * Date: 05/10/17
+ * Time: 16:19
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Karuka</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Hackathon</title>
+    <meta charset="utf-8">
+</head>
+
+
+<body>
