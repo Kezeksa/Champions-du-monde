6476473c68b063c8e0b0dae1e8980b2de117ae15
RequinC <em.leviavant@gmail.com>
2017-10-05 21:57:29 +0200

    modif'sur taille thumbnails

--- public/books.php
+++ public/books.php
@@ -6,22 +6,20 @@
  * Time: 15:52
  */
 include 'header.php';
-    ?>
-    /**
-    * Created by PhpStorm.
-    * User: wilder12
-    * Date: 05/10/17
-    * Time: 15:51
-    */
+?>
 
     <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="..." alt="...">
-                <div class="caption">
-                    <h3>Titre du livre</h3>
-                    <p>...</p>
-                    <p><a href="#" class="btn btn-primary" role="button">Add</a>
+        <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="..." alt="...">
+                        <div class="caption">
+                            <h3>Titre du livre</h3>
+                            <p>...</p>
+                            <p><a href="#" class="btn btn-primary" role="button">Add</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
